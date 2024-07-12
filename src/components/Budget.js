fc4effa6c44e7118f9cb3f8035123c905c5f7bf0
Meghan Kerr <megshannah@hotmail.com>
2024-07-12 00:20:40 -0400

    add currency

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,39 +2,44 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
     const { dispatch } = useContext(AppContext);
     const { expenses } = useContext(AppContext);
-    const { currency } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
     const maxValue = 20000;
+    
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
 
         if(event.target.value > 20000) {
-            alert("The Budget cannot exceed £"+maxValue);
+            alert("The Budget cannot exceed "+ budget.currency + maxValue);
             return;
         }
+
         if (expenses) {
             const totalExpenses = expenses.reduce((total, item) => {
                 return (total = total + item.cost);
             }, 0);
             if( totalExpenses > event.target.value)
             {
-                alert("You cannot reduce the Budget value lower than the already spent amount of £"+totalExpenses);
+                alert("You cannot reduce the Budget value lower than the already spent amount of "+ budget.currency + totalExpenses);
                 return;
             }
         };
-        setNewBudget(event.target.value);
-        dispatch({
-            payload: newBudget
 
+        setNewBudget(event.target.value);
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
         });
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: {currency}{budget} </span>
-<input type="number" step="10" min= "0" max="20000" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
-    );
+    <div className='alert alert-secondary'>
+        <span>Budget: {budget.currency}{budget} </span>
+        <input type="number" step="10" min= "0" max="20000" value={newBudget} onChange={handleBudgetChange}></input>
+    </div>    );
+
 };
+
 export default Budget;
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,29 +1,87 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-const Currency = () => {      
-        const { currency } = useContext(AppContext);
-        const [newCurrency, setNewCurrency] = useState(currency);
-        const { dispatch } = useContext(AppContext);
+const Currency = () => { 
+        const { currency, dispatch } = useContext(AppContext); 
+        const [isOpen, setIsOpen] = useState(false);
 
-        const handleCurrencyChange = (event) => {
-        setNewCurrency(event.target.value);
-        dispatch({
-            payload: newCurrency
+        const getCurrencyLabel = ()=> {
+            switch(currency){
+              case '$' :
+                return '$ Dollar'
+              case '£' :
+                return '£ Pound'
+              case '€' :
+                return '€ Euro'
+              case '₹' :
+                return '₹ Ruppee'
+              default:
+                return ''
+            }
+          }
 
-        });
-    }
+        const handleCurrencyChange = (currency) => {
+           dispatch({
+                type: 'CHG_CURRENCY',
+                payload: currency
+            });
+            setIsOpen(!isOpen);
+        }
     return (
-        <div className='alert alert-secondary'>
-            <span>Currency: </span>
-            <select className="currency-selection" id="inputCurrencyGroupSelect01" onChange={(event) => handleCurrencyChange(event.target.value)} >
-                <option defaultValue>({newCurrency})</option>
-                <option value='$' name="dollar">$ Dollar</option>
-                <option value='£' name="pound">£ Pound</option>
-                <option value='€' name="euro">€ Euro</option>
-                <option value='₹' name="rupee">₹ Rupee</option>
-            </select>
-        </div>
+        <div id="currency-menu" className="dropdown" style={{ cursor: 'pointer' }}>
+        <button
+          id="currency-menu-button"
+          className="btn dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+          style={{ backgroundColor: '#93e399', color: '#fff' }}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          Currency {'('}{getCurrencyLabel()}{')'}
+        </button>
+        <ul className={`custom-menu dropdown-menu ${isOpen ? 'show' : ''} `}
+            style={{ backgroundColor: '#93e399'}}>
+          <li>
+            <button
+              className="dropdown-item"
+              type="button"
+              onClick={() => handleCurrencyChange('$')}
+            >
+             $ Dollar
+            </button>
+          </li>
+          <li>
+            <button
+              className="dropdown-item"
+              type="button"
+              onClick={() => handleCurrencyChange('£')}
+            >
+             £ Pound
+            </button>
+          </li>
+          <li>
+            <button
+              className="dropdown-item"
+              type="button"
+              onClick={() => handleCurrencyChange('€')}
+            >
+             € Euro
+            </button>
+          </li>
+          <li>
+            <button
+              className="dropdown-item"
+              type="button"
+              onClick={() => handleCurrencyChange('₹')}
+            >
+             ₹ Ruppee
+            </button>
+          </li>
+        </ul>
+      </div>
     );
 }
+
+
 
 export default Currency;
